1071f9add8cebf18ca6a85d949f5bc97343d9b51
301prateek <prateek.patil301@gmail.com>
2020-10-18 12:35:37 +0530

    Added: Condition to check winner

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -4,3 +4,17 @@
 
 #Wlecome message
 echo "Welcome Flip Coin Simulation"
+
+#Random function to take two random values.
+coinFlip=$((RANDOM%2))
+tails=0
+heads=1
+
+#Using if condition to display heads or tails as winner
+if [ $coinFlip -eq $heads ]
+then
+	echo "Heads Wins"
+else
+	echo "Tails Wins"
+fi
+
