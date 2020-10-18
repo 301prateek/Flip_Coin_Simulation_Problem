df938232ff3bec189f28d79b05f21391a4d6a8a6
301prateek <prateek.patil301@gmail.com>
2020-10-18 12:50:26 +0530

    Added: For loop to count number of times won

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -18,3 +18,19 @@ else
 	echo "Tails Wins"
 fi
 
+#Loop to show number of times head and tail has won
+headCount=0
+tailCount=0
+for(( i=1; i<=10; i++ ))
+do
+	coin=$((RANDOM%2))
+	if [ $coin -eq $heads ]
+	then
+		headCount=$(($headCount + 1))
+	else
+		tailCount=$(($tailCount + 1))
+	fi
+done
+echo "Heads won: $headCount times"
+echo "Tails won: $tailCount times"
+
