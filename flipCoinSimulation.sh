c7e8edf6632d33a9cc0b3831dae8ba40a4d9f8a3
301prateek <prateek.patil301@gmail.com>
2020-10-18 14:07:10 +0530

    Added: While loop until either wins 21 times

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -34,3 +34,34 @@ done
 echo "Heads won: $headCount times"
 echo "Tails won: $tailCount times"
 
+heads=0
+tails=0
+while(true)
+do
+	coin=$((RANDOM%2))
+	
+	if [ $coin -eq 1 -a $heads -le 21 ]
+	then
+		heads=$(($heads + 1))
+
+	elif [ $coin -eq 0 -a $tails -le 21 ]
+	then
+		tails=$(($tails + 1))
+	fi
+
+	if [ $heads -eq 21 ]
+	then
+		diff=$(($heads - $tails))
+		echo "Heads won by points: $diff"
+		echo "Heads wins 21 times"
+		break
+	elif [ $tails -eq 21 ]
+	then
+		diff=$(($tails - $heads))
+		echo "tails won by point: $diff"
+		echo "Tails wins 21 times"
+		break
+	fi
+
+done
+
